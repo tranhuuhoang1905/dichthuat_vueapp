fa7be7cc1290f58ff56cb1099d9e4356a85d9898
daovantrung12092001 <daovantrung12092001@gmail.com>
2023-03-23 10:41:33 +0700

    style form login, register and home page

--- resources/js/components/account/Login.vue
+++ resources/js/components/account/Login.vue
@@ -1,6 +1,6 @@
 <script>
 import { required, email } from "vuelidate/lib/validators";
-import { mapGetters, mapMutations, mapActions } from 'vuex';
+import { mapGetters, mapMutations, mapActions } from "vuex";
 // import {
 //   authMethods,
 //   authFackMethods,
@@ -8,179 +8,219 @@ import { mapGetters, mapMutations, mapActions } from 'vuex';
 // } from "@/state/helpers";
 
 export default {
-    data() {
-        return {
-            loginData: {
-                email: 'admin@gmail.com',
-                password: '123456',
-                submitted: false
-            },
-            email: "admin@gmail.com",
-            password: "123456",
-            submitted: false
-        };
-    },
-    computed: {
-        // ...mapGetters([
-        //     'getUser',
-        //     'getAge',            
-        // ])
-        ...mapGetters({
-            loginResponse: 'getLoginResponse',
-            getUser: 'getUser',
-            getAge: 'getAge',
-        })
-    },
-    created() {
-        document.body.classList.add("auth-body-bg");
-    },
-    validations: {
-        email: { required, email },
-        password: { required }
-    },
-    methods: {
-        // Try to log the user in with the username
-        // and password they provided.
-        // tryToLogIn() {
-        //     this.submitted = true;
-        //     // stop here if form is invalid
-        //     // this.$v.$touch();
-        //     // console.log(this.email,this.password)
-        //     // console.log(this)
-        // },
-        ...mapActions(['login']),
+  data() {
+    return {
+      loginData: {
+        email: "admin@gmail.com",
+        password: "123456",
+        submitted: false,
+      },
+      email: "admin@gmail.com",
+      password: "123456",
+      submitted: false,
+    };
+  },
+  computed: {
+    // ...mapGetters([
+    //     'getUser',
+    //     'getAge',
+    // ])
+    ...mapGetters({
+      loginResponse: "getLoginResponse",
+      getUser: "getUser",
+      getAge: "getAge",
+    }),
+  },
+  created() {
+    document.body.classList.add("auth-body-bg");
+  },
+  validations: {
+    email: { required, email },
+    password: { required },
+  },
+  methods: {
+    // Try to log the user in with the username
+    // and password they provided.
+    // tryToLogIn() {
+    //     this.submitted = true;
+    //     // stop here if form is invalid
+    //     // this.$v.$touch();
+    //     // console.log(this.email,this.password)
+    //     // console.log(this)
+    // },
+    ...mapActions(["login"]),
 
-        loginSubmit() {
-            this.login(this.loginData);
-        },
-
-        showPassword() {
-            let password = document.querySelector('#password');
-            if (password.type == 'password') {
-                password.type = 'text';
-            } else {
-                password.type = 'password';
-            }
-        },
+    loginSubmit() {
+      this.login(this.loginData);
     },
-    mounted() {
-        console.log(this.loginResponse)
-    }
+
+    showPassword() {
+      let password = document.querySelector("#password");
+      if (password.type == "password") {
+        password.type = "text";
+      } else {
+        password.type = "password";
+      }
+    },
+  },
+  mounted() {
+    console.log(this.loginResponse);
+  },
 };
 </script>
 <template>
-    <div>
-        <div class="home-btn d-none d-sm-block">
-            <a href="/">
-                <i class="mdi mdi-home-variant h2 text-white"></i>
-            </a>
-        </div>
-        <div>
-            <div class="container-fluid p-0">
-                <div class="row no-gutters">
-                    <div class="col-lg-4">
-                        <div class="authentication-page-content p-4 d-flex align-items-center min-vh-100">
-                            <div class="w-100">
-                                <div class="row justify-content-center">
-                                    <div class="col-lg-9">
-                                        <div>
-                                        <div class="text-center">
-                                                <div>
-                                                    <a href="/" class="logo">
-                                                        <img src="/assets/images/logo-dark.png" height="20" alt="logo" />
-                                                    </a>
-                                                </div>
-
-                                                <h4 class="font-size-18 mt-4">Welcome Back !</h4>
-                                                <p class="text-muted">Sign in to continue to Nazox.</p>
-                                                <p>NewName: {{ $store.getters['myModule/getAge'] }}</p>
-                                                <p>UserName: {{ $store.getters['myModule/getUser'] }}</p>
-                                            </div>
-
-                                            <b-alert variant="danger" class="mt-3">
-                                            </b-alert>
-
-                                            <div class="p-2 mt-5">
-                                                <form class="form-horizontal" @submit.prevent="loginSubmit">
-                                                    <div class="form-group auth-form-group-custom mb-4">
-                                                        <i class="ri-mail-line auti-custom-input-icon"></i>
-                                                        <label for="email">Email</label>
-                                                        <input type="email" v-model="loginData.email" class="form-control"
-                                                            id="email" placeholder="Enter email"
-                                                            :class="{ 'is-invalid': submitted && $v.email.$error }" />
-                                                        <div v-if="submitted && $v.email.$error" class="invalid-feedback">
-                                                            <span v-if="!$v.email.required">Email is required.</span>
-                                                            <span v-if="!$v.email.email">Please enter valid email.</span>
-                                                        </div>
-                                                    </div>
-
-                                                    <div class="form-group auth-form-group-custom mb-4">
-                                                        <i class="ri-lock-2-line auti-custom-input-icon"></i>
-                                                        <label for="userpassword">Password</label>
-                                                        <input v-model="loginData.password" type="password"
-                                                            class="form-control" id="userpassword"
-                                                            placeholder="Enter password"
-                                                            :class="{ 'is-invalid': submitted && $v.password.$error }" />
-                                                        <div v-if="submitted && !$v.password.required"
-                                                            class="invalid-feedback">Password is required.</div>
-                                                    </div>
-
-                                                    <div class="custom-control custom-checkbox">
-
-                                                        <label class="custom-control-label" for="customControlInline">
-                                                            <input type="checkbox" class="custom-control-input"
-                                                                id="customControlInline" />
-                                                            Remember me
-                                                        </label>
-                                                    </div>
-
-                                                    <div class="mt-4 text-center">
-                                                        <button class="btn btn-primary w-md waves-effect waves-light"
-                                                            type="submit">Log In</button>
-                                                    </div>
-
-                                                    <div class="mt-4 text-center">
-                                                        <!-- tag="a" -->
-                                                        <router-link to="/forgot-password" class="text-muted">
-                                                            <i class="mdi mdi-lock mr-1"></i> Forgot your password?
-                                                        </router-link>
-                                                    </div>
-                                                </form>
-                                            </div>
-
-                                            <div class="mt-5 text-center">
-                                                <p>
-                                                    Don't have an account ?
-                                                    <!-- tag="a" -->
-                                                    <a href="/register" class="font-weight-medium text-primary">Đăng ký</a>
-                                                    <!-- <router-link :to="{ name: 'register' }"
-                                                                                                                class="font-weight-medium text-primary">Register</router-link> -->
-                                                </p>
-                                                <p>
-                                                    © 2020 Nazox. Crafted with
-                                                    <i class="mdi mdi-heart text-danger"></i> by Themesdesign
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-8">
-                        <div class="authentication-bg">
-                            <div class="bg-overlay"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="login_register">
+    <div class="home-btn d-none d-sm-block">
+      <a href="/">
+        <i class="mdi mdi-home-variant h2 text-white"></i>
+      </a>
     </div>
-</template>
+    <div>
+      <div class="container-fluid p-0">
+        <div class="row no-gutters">
+          <div class="col-lg-4">
+            <div
+              class="authentication-page-content d-flex align-items-center min-vh-100"
+            >
+              <div class="w-100">
+                <div class="row justify-content-center">
+                  <div class="col-lg-9">
+                    <div>
+                      <div class="text-center">
+                        <div>
+                          <a href="/" class="logo">
+                            <img
+                              src="/assets/images/logo-dark.png"
+                              height="20"
+                              alt="logo"
+                            />
+                          </a>
+                        </div>
 
-<style>
-.custom-control-input {
-    height: 17px;
-    width: 30px !important;
-}
-</style>
+                        <h4 class="font-size-18 mt-4">Welcome Back !</h4>
+                        <p class="text-muted">Sign in to continue to Nazox.</p>
+                        <p>NewName: {{ $store.getters['myModule/getAge'] }}</p>
+                        <p>UserName: {{ $store.getters['myModule/getUser'] }}</p>
+                      </div>
+
+                      <b-alert variant="danger" class="mt-3"> </b-alert>
+
+                      <div class="p-2 mt-5">
+                        <form
+                          class="form-horizontal"
+                          @submit.prevent="loginSubmit"
+                        >
+                          <div class="form-group auth-form-group-custom mb-4">
+                            <i class="ri-mail-line auti-custom-input-icon"></i>
+                            <label for="email">Email</label>
+                            <input
+                              type="email"
+                              v-model="loginData.email"
+                              class="form-control"
+                              id="email"
+                              placeholder="Enter email"
+                              :class="{
+                                'is-invalid': submitted && $v.email.$error,
+                              }"
+                            />
+                            <div
+                              v-if="submitted && $v.email.$error"
+                              class="invalid-feedback"
+                            >
+                              <span v-if="!$v.email.required"
+                                >Email is required.</span
+                              >
+                              <span v-if="!$v.email.email"
+                                >Please enter valid email.</span
+                              >
+                            </div>
+                          </div>
+
+                          <div class="form-group auth-form-group-custom mb-4">
+                            <i
+                              class="ri-lock-2-line auti-custom-input-icon"
+                            ></i>
+                            <label for="userpassword">Password</label>
+                            <input
+                              v-model="loginData.password"
+                              type="password"
+                              class="form-control"
+                              id="userpassword"
+                              placeholder="Enter password"
+                              :class="{
+                                'is-invalid': submitted && $v.password.$error,
+                              }"
+                            />
+                            <div
+                              v-if="submitted && !$v.password.required"
+                              class="invalid-feedback"
+                            >
+                              Password is required.
+                            </div>
+                          </div>
+
+                          <div class="custom-control custom-checkbox">
+                            <label
+                              class="custom-control-label"
+                              for="customControlInline"
+                            >
+                              <input
+                                type="checkbox"
+                                class="custom-control-input"
+                                id="customControlInline"
+                              />
+                              Remember me
+                            </label>
+                          </div>
+
+                          <div class="mt-4 text-center">
+                            <button
+                              class="btn btn-primary w-md waves-effect waves-light"
+                              type="submit"
+                            >
+                              Log In
+                            </button>
+                          </div>
+
+                          <div class="mt-4 text-center">
+                            <!-- tag="a" -->
+                            <router-link
+                              to="/forgot-password"
+                              class="text-muted"
+                            >
+                              <i class="mdi mdi-lock mr-1"></i> Forgot your
+                              password?
+                            </router-link>
+                          </div>
+                        </form>
+                      </div>
+
+                      <div class="mt-5 text-center">
+                        <p>
+                          Don't have an account ?
+                          <!-- tag="a" -->
+                          <!-- <a href="/register" class="font-weight-medium text-primary">Đăng ký</a> -->
+                          <router-link :to="{ name: 'register' }" class="font-weight-medium text-primary">Đăng Ký</router-link>
+                        </p>
+                        <p>
+                          © 2020 Nazox. Crafted with
+                          <i class="mdi mdi-heart text-danger"></i> by
+                          Themesdesign
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-8">
+            <div class="authentication-bg">
+              <div class="bg-overlay"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- resources/js/components/home/view/NavbarView.vue
+++ resources/js/components/home/view/NavbarView.vue
@@ -1,73 +1,45 @@
 <template>
-    <!-- <div class="container-xl"> -->
+  <div class="navbar_home">
     <nav class="navbar navbar-expand-lg navbar-light bg-main shadow-sm">
-        <div class="collapse navbar-collapse d-flex justify-content-between">
-            <div class="text-center py-4 px-5">
-                <span class="text-white">Laravel Home Example</span>
-            </div>
-            <div class="navbar-nav px-5 align-items-center">
-                <router-link to="/admin" class="navbar_menu nav-item nav-link py-4 text-white">
-                    Dashboard
-                    <div class="border_link"></div>
-                </router-link>
-                <button class="btn-logout bg-white text-black py-2 px-4 rounded-1 border border-0" @click="logoutSubmit">
-                    Logout
-                </button>
-            </div>
+      <div class="collapse navbar-collapse d-flex justify-content-between">
+        <div class="text-center py-4 px-5">
+          <span class="text-white">Laravel Home Example</span>
         </div>
+        <div class="navbar-nav px-5 align-items-center">
+          <router-link
+            to="/admin"
+            class="navbar_menu nav-item nav-link py-4 text-white"
+          >
+            Dashboard
+            <div class="border_link"></div>
+          </router-link>
+          <button
+            class="btn-logout bg-white text-black py-2 px-4 rounded-1 border border-0"
+            @click="logoutSubmit"
+          >
+            Logout
+          </button>
+        </div>
+      </div>
     </nav>
-    <!-- </div> -->
+  </div>
 </template>
 
 <script>
-import { mapGetters, mapMutations, mapActions } from 'vuex';
+import { mapGetters, mapMutations, mapActions } from "vuex";
 export default {
-    methods: {
-        ...mapActions(['logout']),
-        logoutSubmit() {
-            this.logout(this.loginData);
-        },
-        reloadPage() {
-            location.reload();
-        },
+  methods: {
+    ...mapActions(["logout"]),
+    logoutSubmit() {
+      this.logout(this.loginData);
     },
-    computed: {
+    reloadPage() {
+      location.reload();
     },
-    mounted() {
-        console.log(this.loginResponse)
-    }
-}
+  },
+  computed: {},
+  mounted() {
+    console.log(this.loginResponse);
+  },
+};
 </script>
-<style>
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-.bg-main {
-    background-color: #091e42f2;
-}
-
-.btn-logout:hover {
-    color: blue !important
-}
-
-.navbar_menu {
-    margin-right: 30px;
-    position: relative;
-}
-
-.navbar_menu:hover .border_link {
-    display: block;
-}
-
-.border_link {
-    width: 100%;
-    position: absolute;
-    height: 4px;
-    background: white !important;
-    bottom: 0;
-    left: 0;
-    display: none;
-}</style>
