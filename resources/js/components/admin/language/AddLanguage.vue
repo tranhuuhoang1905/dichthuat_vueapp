748d2d18704e97cb21ed160bd06d8a9a946eb487
daovantrung12092001 <daovantrung12092001@gmail.com>
2023-03-24 10:27:10 +0700

    style form add and edit language page admin

--- resources/js/components/admin/language/AddLanguage.vue
+++ resources/js/components/admin/language/AddLanguage.vue
@@ -1,52 +1,71 @@
 <template>
-    <div class="add-language">
-        <h3 class="text-center">Add Language</h3>
-        <div class="row">
-            <div class="col-md-6">
-                <form @submit.prevent="addLanguage">
-                    <div class="form-group">
-                        <label for="language-name">Language name</label>
-                        <input id="language-name" type="text" placeholder="Enter language name" class="form-control"
-                            v-model="language.name" />
-                    </div>
-                    <div class="form-group">
-                        <label for="language-description">Description</label>
-                        <input id="language-description" type="text" placeholder="Enter description" class="form-control"
-                            v-model="language.description" />
-                    </div>
-                    <button type="submit" class="btn btn-primary">Add Language</button>
-                </form>
-            </div>
+  <div class="add-edit-language">
+    <div class="row">
+      <div class="row d-flex justify-content-center">
+        <div class="card show border border-0">
+        <div class="card-body">
+          <h4 class="card-title text-center fs-4">Add Language</h4>
+          <div class="col-md-12 d-flex flex-column align-items-center">
+            <form @submit.prevent="addLanguage">
+              <div class="form-group">
+                <label for="language-name">Language name</label>
+                <input
+                  id="language-name"
+                  type="text"
+                  placeholder="Enter language name"
+                  class="form-control"
+                  v-model="language.name"
+                />
+              </div>
+              <div class="form-group">
+                <label for="language-description">Description</label>
+                <textarea
+                  id="language-description"
+                  type="text"
+                  placeholder="Enter description"
+                  class="form-control"
+                  rows="7"
+                  v-model="language.description"
+                ></textarea>
+              </div>
+              <div class="d-flex justify-content-center">
+                <button type="submit" class="btn btn-add-edit">
+                  Add Language
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    data() {
-        return {
-            language: {}
-        };
+  data() {
+    return {
+      language: {},
+    };
+  },
+  methods: {
+    addLanguage() {
+      // Gọi API để thêm ngôn ngữ mới
+      this.axios
+        .post("/api/language/add", this.language)
+        .then((response) => {
+          // Nếu thành công, đưa người dùng đến trang danh sách các ngôn ngữ
+          this.$router.push({ name: "languages" });
+        })
+        .catch((error) => {
+          // Nếu không thành công, hiển thị thông báo lỗi
+          console.log(error);
+          alert("Failed to add language");
+        });
     },
-    methods: {
-        addLanguage() {
-            // Gọi API để thêm ngôn ngữ mới
-            this.axios
-                .post("/api/language/add", this.language)
-                .then(response => {
-                    // Nếu thành công, đưa người dùng đến trang danh sách các ngôn ngữ
-                    this.$router.push({ name: "languages" });
-                })
-                .catch(error => {
-                    // Nếu không thành công, hiển thị thông báo lỗi
-                    console.log(error);
-                    alert("Failed to add language");
-                });
-        }
-    }
+  },
 };
 </script>
-<style scoped>
-.add-language {
-    margin: 20px;
-}
+<style lang="scss">
+
 </style> 
