6162c1fdf6e0766e2eec7afa63d4f0d2f5c7ec20
hoangtran <tranhuuhoang190594@gmail.com>
2023-04-06 17:21:44 +0700

    fix add language

--- app/Http/Controllers/API/WordsController.php
+++ app/Http/Controllers/API/WordsController.php
@@ -70,7 +70,7 @@ class WordsController extends Controller
         $languageId = 1;
         $keyword = $request->input('keyword');
         $words = DB::table('words')
-            ->select('id', 'word') // Chỉ định các cột cần lấy
+            ->select('id', 'word','description','language_id') // Chỉ định các cột cần lấy
             ->where('word', 'like', '%'.$keyword.'%')
             ->where('language_id', $languageId)
             ->orderByRaw('LENGTH(word)')
